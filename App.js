1df320618b2934b2311005d6beb3bd448ae46242
Irsael <debanjo31@gmail.com>
2024-06-02 14:31:04 +0100

    feat: Add tasks to the App component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/task";
 
 export default function App() {
   return (
@@ -8,6 +9,9 @@ export default function App() {
         <Text style={styles.sectionHeader}>Todays Task</Text>
         <View style={styles.items}>
           {/* This is where the tasks will go */}
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
+          <Task text={"Task 3"} />
         </View>
       </View>
     </View>
@@ -26,5 +30,8 @@ const styles = StyleSheet.create({
   sectionHeader: {
     fontSize: 24,
     fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
   },
 });
